8a7db887e53e3a00590717a65f1741f730710432
Cherukara <matt_cherukara@hotmail.com>
2026-05-20 10:36:46 +0100

    specific run shell file

--- using_Histo_uSim/run_full_fitting_exvivomouse.sh
+++ using_Histo_uSim/run_full_fitting_exvivomouse.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-protocolname="EXVIVO_SHELL2_RUN2"
+protocolname="EXVIVO_SHELL5_RUN1"
 
 echo "Finding closest protocol, getting the subsets and processing .nii files"
 
 python get_closest_scheme.py \
     --protocol-name $protocolname \
     --ref-signal all_signals_all_substrates.npy \
-    --dwi ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell2_denoised_sphmean.nii \
-    --bval ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell2.bval \
-    --gdur ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell2.gdur \
-    --gsep ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell2.gsep \
+    --dwi ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_denoised_shell5_sphmean.nii \
+    --bval ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell5.bval \
+    --gdur ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell5.gdur \
+    --gsep ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_TE28_shell5.gsep \
     --bval-threshold 20 \
-    --vasc-threshold 300 \
+    --vasc-threshold 100 \
     --noise ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_noise.nii \
 
 echo "Selecting parameter configuration: fin vCS_cyl D0in D0ex kappa"
@@ -38,5 +38,5 @@ python mri2micro_dictml.py \
     --ncpu 10 \
     --reg "2,0.01" \
     --noise ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_noise_normalized.nii \
-    --mask ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_auto_mask.nii \
+    --mask ~/Documents/SimProjects/Data_ERCExVivo/sub-01/dwi_singleslice_mask.nii \
     protocols/$protocolname/fitting/Histo_uSim
